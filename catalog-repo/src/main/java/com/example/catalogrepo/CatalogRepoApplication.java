67c333993f377273f775145a6a35ca9a47a94f5b
Evgenii Viriasov <markine087@gmail.com>
2025-02-25 20:34:01 +0300

    create catalog-repo

---
+++ catalog-repo/src/main/java/com/example/catalogrepo/CatalogRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogRepoApplication.class, args);
+    }
+
+}
