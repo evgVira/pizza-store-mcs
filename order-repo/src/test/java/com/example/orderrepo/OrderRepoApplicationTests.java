dec13349cd5efafe2c2974081edf7ba687e6c52d
Evgenii Viriasov <markine087@gmail.com>
2025-02-27 10:10:29 +0300

    create order-repo

---
+++ order-repo/src/test/java/com/example/orderrepo/OrderRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
