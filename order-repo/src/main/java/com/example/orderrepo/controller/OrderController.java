9a1442dc744f46258ae5cc9532e7eff5fa864402
Evgenii Viriasov <eviriasovv@gmail.com>
2025-02-27 20:11:37 +0300

    added userId mapping

--- order-repo/src/main/java/com/example/orderrepo/controller/OrderController.java
+++ order-repo/src/main/java/com/example/orderrepo/controller/OrderController.java
@@ -4,6 +4,8 @@ import com.example.orderrepo.dto.OrderInfoResponseDto;
 import com.example.orderrepo.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -19,14 +21,20 @@ public class OrderController {
     @PostMapping("/create/pizzas/{pizzaId}/totalAmount/{totalAmount}")
     @ResponseStatus(HttpStatus.CREATED)
     public OrderInfoResponseDto createOrder(@PathVariable("pizzaId") UUID pizzaId,
-                                            @PathVariable("totalAmount") BigDecimal totalAmount) {
-        return orderService.createOrder(UUID.randomUUID(), pizzaId, totalAmount);
+                                            @PathVariable("totalAmount") BigDecimal totalAmount, Authentication authentication) {
+        return orderService.createOrder(getUserId(authentication), pizzaId, totalAmount);
     }
 
     @DeleteMapping("/delete/{orderId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteOrder(@PathVariable("orderId") UUID orderId) {
         orderService.deleteOrder(orderId);
+    }
+
+    private static UUID getUserId(Authentication authentication) {
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        String userIdAsString = jwt.getClaimAsString("sub");
+        return UUID.fromString(userIdAsString);
     }
 
 }
