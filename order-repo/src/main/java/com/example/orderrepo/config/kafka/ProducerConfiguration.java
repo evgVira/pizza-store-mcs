60b00e5f8ec020c9313598af028e967340118f99
Evgenii Viriasov <eviriasovv@gmail.com>
2025-02-28 21:12:51 +0300

    add kafka config

---
+++ order-repo/src/main/java/com/example/orderrepo/config/kafka/ProducerConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.orderrepo.config.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProducerConfiguration {
+
+    private String BOOTSTRAP_SERVER = "localhost:9092";
+
+    private final String ORDERS_TOPIC = "orders-topic";
+
+    private final static int RETRIES = 3;
+
+    private final static String ACKS = "all";
+
+    private final static int BATCH_SIZE = 16384;
+
+    private final static int LINGER_MS = 5;
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder
+                .name(ORDERS_TOPIC)
+                .replicas(1)
+                .partitions(2)
+                .build();
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.RETRIES_CONFIG, RETRIES);
+        props.put(ProducerConfig.ACKS_CONFIG, ACKS);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, BATCH_SIZE);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, LINGER_MS);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
