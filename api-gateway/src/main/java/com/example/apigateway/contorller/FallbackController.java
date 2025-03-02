3f6c694bf48499f10ab435522a57d54e4edbecfb
Evgenii Viriasov <eviriasovv@gmail.com>
2025-03-02 15:13:29 +0300

    add circuitbreaker and actuator

---
+++ api-gateway/src/main/java/com/example/apigateway/contorller/FallbackController.java
@@ -0,0 +1,25 @@
+package com.example.apigateway.contorller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    private static final String SERVER_ERROR_MESSAGE = "Server is not available, please try later!";
+
+    @RequestMapping(value = "/catalog", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+            RequestMethod.PATCH, RequestMethod.DELETE})
+    public String catalogFallback() {
+        return SERVER_ERROR_MESSAGE;
+    }
+
+    @RequestMapping(value = "/order", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+            RequestMethod.PATCH, RequestMethod.DELETE})
+    public String orderFallback() {
+        return SERVER_ERROR_MESSAGE;
+    }
+
+}
