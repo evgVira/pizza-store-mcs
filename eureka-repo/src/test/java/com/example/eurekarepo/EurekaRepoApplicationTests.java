47f24a3efe7cde9f4bcd3c292c9659cfa2513faa
Evgenii Viriasov <eviriasovv@gmail.com>
2025-03-02 11:36:06 +0300

    added eureka server

---
+++ eureka-repo/src/test/java/com/example/eurekarepo/EurekaRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekarepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
